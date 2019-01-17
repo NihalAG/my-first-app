124e17816b989744f3aaff37dbaa7bad5228fb81
NihalAG <nihal.gesudraz@gmail.com>
2019-01-16 23:09:28 -0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { Button, Row, Col } from 'reactstrap'
 
 class App extends Component {
+  state = {
+    clickerValue: 0
+  }
+
+  incrementClicker = () => {
+    this.setState((prevState) => ({clickerValue: prevState.clickerValue + 1}))
+  }
+
+  decrementClicker = () => {
+    this.setState((prevState) => ({clickerValue: prevState.clickerValue - 1}))
+  }
+
+  resetClicker = () => {
+    this.setState((prevState) => ({clickerValue: 0}))
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="clicker-container">
+        <Row>
+          <Col sm={{ size: 3, offset: 4}}>
+            <div className="jumbotron">
+              <h1>
+                <center>
+                  {this.state.clickerValue}
+                </center>
+              </h1>
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col sm={{ size: 'auto', offset: 4 }}>
+            <Button onClick={this.incrementClicker} color="success">Add</Button> 
+          </Col>
+          &nbsp; 
+          <Col sm={{ size: 'auto', offset: 1 }}>
+            <Button onClick={this.decrementClicker} color="danger">Reduce</Button>
+          </Col>
+        </Row>
+        <br/>
+        <Row>
+          <Col sm={{ size: 1, offset: 5 }}>
+            <Button onClick={this.resetClicker} color="warning">Reset</Button>
+          </Col>
+        </Row>
       </div>
     );
   }
